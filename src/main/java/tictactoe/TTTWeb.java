3a7364b823dca5bda64fe07863ccda2a6631c90e
Atli Saevar <atli@i-6f102c8d.greenqloud.internal>
2014-10-29 18:56:21 +0000

    sparka upp leikinn

---
+++ src/main/java/tictactoe/TTTWeb.java
@@ -0,0 +1,71 @@
+package tictactoe;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
+public class TTTWeb implements SparkApplication {
+
+	public static void main(String args[]) {
+		staticFileLocation("/public");
+		SparkApplication tttweb = new TTTWeb();
+        	String port = System.getenv("PORT");
+        	if (port != null) {
+        		setPort(Integer.valueOf(port));
+        	}
+        	tttweb.init();
+	}
+
+	public void init(){
+        	final TTTInterface vidmot = new TTTInterface();
+		final TicTacToe mylla = new TicTacToe();
+
+		post(new Route("/welcome"){
+			@Override
+			public Object handle(Request request, Response response){
+				String welcome = vidmot.welcome();
+				return welcome;
+			}
+		});
+
+		post(new Route("/instructions"){
+                        @Override
+                        public Object handle(Request request, Response response){
+                                String instr = vidmot.instructions();
+                                return instr;
+                        }
+                });
+
+        	post(new Route("/start"){
+            		@Override
+            		public Object handle(Request request, Response response){
+                		String board = mylla.drawBoard() + mylla.getPlayer() + " á leik";
+                		return board;
+            		}
+        	});
+
+		post(new Route("/play"){
+                        @Override
+                        public Object handle(Request request, Response response){
+				String input = request.queryParams("input");
+				mylla.updateBoard(input);
+                                String board = mylla.drawBoard() + mylla.getPlayer() + " á leik";
+				if (mylla.isTie)
+					return board + "\nJafntefli!";
+				if (mylla.isFinished())
+					return board + mylla.getPlayer() + " hefur unnið!";
+                                return board;
+                        }
+                });
+
+		post(new Route("/play"){
+                        @Override
+                        public Object handle(Request request, Response response){
+                                String input = request.queryParams("input");
+                                mylla.updateBoard(input);
+                                String board = mylla.drawBoard() + mylla.getPlayer() + " á leik";
+                                return board;
+                        }
+                });
+    	}
+}
